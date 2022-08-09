5a9a6dea6481aa3cfdf041ac334df97c558d585d
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2022-08-08 21:09:24 -0400

    got run dev working

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+	},
+	preprocess: 
+		preprocess({
+			postcss: {
+				plugins: [
+					tailwind,
+					autoprefixer
+				]
+			}
+		}),
+	
 };
 
 export default config;
